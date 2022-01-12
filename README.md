ed92b120fb8702e311fd88215ac98a8aaeda8d6e
kukabi <kutsalbilgin@gmail.com>
2022-01-12 18:16:08 +0300

    Project init.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# subvt-ios-data-access
-iOS framework to communicate with the SubVT backend.
+<p align="center">
+	<img width="400" src="https://raw.githubusercontent.com/helikon-labs/subvt/main/assets/design/logo/subvt_logo_blue.png">
+</p>
+
+SubVT (Substrate Validator Toolkit) data access framework for iOS. Please visit the top-level [repository](https://github.com/helikon-labs/subvt) for project information.
---
+++ SubVT-Data/SubVT_Data.h
@@ -0,0 +1,18 @@
+//
+//  SubVT_Data.h
+//  SubVT-Data
+//
+//  Created by Kutsal Kaan Bilgin on 12.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SubVT_Data.
+FOUNDATION_EXPORT double SubVT_DataVersionNumber;
+
+//! Project version string for SubVT_Data.
+FOUNDATION_EXPORT const unsigned char SubVT_DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubVT_Data/PublicHeader.h>
+
+
