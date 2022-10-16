52d1253f82dc706c84ae8452f38b46c56ec697aa
kukabi <kutsalbilgin@gmail.com>
2022-10-16 11:56:53 +0300

    Add the new isEnabled field to NotificationType.

--- Sources/SubVTData/Model/App/NotificationType.swift
+++ Sources/SubVTData/Model/App/NotificationType.swift
@@ -3,13 +3,16 @@
  */
 public struct NotificationType: Hashable {
     public let code: String
+    public let isEnabled: Bool
     public let paramTypes: [NotificationParamType]
     
     public init(
         code: String,
+        isEnabled: Bool,
         paramTypes: [NotificationParamType]
     ) {
         self.code = code
+        self.isEnabled = isEnabled
         self.paramTypes = paramTypes
     }
 }
