d5fee65adbfff5b716f13aa259fd894cb1611a68
kukabi <kutsalbilgin@gmail.com>
2024-06-24 12:29:17 +0300

    Added monthly income report public init.

--- Sources/SubVTData/Model/Report/MonthlyIncomeReport.swift
+++ Sources/SubVTData/Model/Report/MonthlyIncomeReport.swift
@@ -4,6 +4,16 @@ public struct MonthlyIncome: Hashable {
     public let year: UInt32
     public let month: UInt32
     public let income: Decimal
+    
+    public init(
+        year: UInt32,
+        month: UInt32,
+        income: Decimal
+    ) {
+        self.year = year
+        self.month = month
+        self.income = income
+    }
 }
 
 extension MonthlyIncome: Codable {}
@@ -12,6 +22,16 @@ public struct MonthlyIncomeReport {
     public let rewardee: AccountId
     public let tokenSymbol: String
     public let monthlyIncome: [MonthlyIncome]
+    
+    public init(
+        rewardee: AccountId,
+        tokenSymbol: String,
+        monthlyIncome: [MonthlyIncome]
+    ) {
+        self.rewardee = rewardee
+        self.tokenSymbol = tokenSymbol
+        self.monthlyIncome = monthlyIncome
+    }
 }
 
 extension MonthlyIncomeReport: Codable {}
